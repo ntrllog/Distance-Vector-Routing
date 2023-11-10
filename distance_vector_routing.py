6abaee7917816c91af6c696b653110369b082355
Steven100695 <112973112+Steven100695@users.noreply.github.com>
2023-11-10 02:43:21 -0800

    user_input and commands functions

--- distance_vector_routing.py
+++ distance_vector_routing.py
@@ -1,26 +1,44 @@
 from program_manager import ProgramManager
 from server_object import ServerObject
 import threading
+import socket
 
-if __name__ == '__main__':
-    program_manager = ProgramManager()
+serverIP = "xxx.xxx.xxx"
+serverPort = 0000
 
-    # ask for user command (only accept "server" at the beginning)
+user_input = None
+startupCmdEntered = False
+running = True
 
-    # read topology file
 
-    # initialize this server's distance vector
+def get_user_input():
+    global user_input, startupCmdEntered, running
+    while running:
+        user_input = input('Enter a command: ')
 
-    # start UDP socket (using this server's ip and port) on another thread
-    exit_event = threading.Event()
+        if user_input.startswith("server -t") and not startupCmdEntered:
+            startupCmdEntered = True
+            execute_command(user_input)
+        elif not user_input.startswith("server -t") and not startupCmdEntered:
+            print("COMMAND IS NOT AN AVAILABLE COMMAND")
+            print("Enter 'server -t <topology-file-name> -i <routing-update-interval>' at the startup.")
+        elif user_input.startswith("server -t") and startupCmdEntered:
+            print("SERVER already used")
+        else:
+            execute_command(user_input)  # call execute_command here debug
 
-    # start timer on another thread
 
-    # ask for user input
+def execute_command(commands):
     while True:
         try:
-            command = input('Enter a command: ').split()
-            if command[0] == 'update':
+            command = commands.split()
+            if command[0] == 'help':
+                print("Print commands list here")  # debug
+                pass
+            elif command[0] == 'server':
+                print("Read topology file and update routing every x seconds")  # debug
+                pass
+            elif command[0] == 'update':
                 server_id_1, server_id_2, link_cost = int(command[1]), int(command[2]), int(command[3])
                 pass
             elif command[0] == 'step':
@@ -43,3 +61,23 @@ if __name__ == '__main__':
         except ValueError:
             # args to update or disable command are not numbers
             pass
+
+
+if __name__ == '__main__':
+    program_manager = ProgramManager()
+
+    # ask for user command (only accept "server" at the beginning)
+    print("Enter 'server -t <topology-file-name> -i <routing-update-interval>' to startup the server.")
+    input_thread = threading.Thread(target=get_user_input)
+    input_thread.start()
+    # read topology file
+
+    # initialize this server's distance vector
+
+    # start UDP socket (using this server's ip and port) on another thread
+    exit_event = threading.Event()
+
+    # start timer on another thread
+
+    # ask for user input
+
