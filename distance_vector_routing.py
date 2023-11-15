f2406d1fbb49162767b236c7cab55563530fce78
Steven100695 <112973112+Steven100695@users.noreply.github.com>
2023-11-15 04:44:23 -0800

    server commend and program_manager.init_topology(file_name)

--- distance_vector_routing.py
+++ distance_vector_routing.py
@@ -1,76 +1,64 @@
 from program_manager import ProgramManager
 from server_object import ServerObject
 import threading
-import socket
-
-serverIP = "xxx.xxx.xxx"
-serverPort = 0000
-
-user_input = None
-startupCmdEntered = False
-running = True
+import os
 
 
-def get_user_input():
-    global user_input, startupCmdEntered, running
-    while running:
-        user_input = input('Enter a command: ')
+def validate_command(server_command):
+    parts = server_command.split()
 
-        if user_input.startswith("server -t") and not startupCmdEntered:
-            startupCmdEntered = True
-            execute_command(user_input)
-        elif not user_input.startswith("server -t") and not startupCmdEntered:
-            print("COMMAND IS NOT AN AVAILABLE COMMAND")
-            print("Enter 'server -t <topology-file-name> -i <routing-update-interval>' at the startup.")
-        elif user_input.startswith("server -t") and startupCmdEntered:
-            print("SERVER already used")
-        else:
-            execute_command(user_input)  # call execute_command here debug
+    # Check if the command starts with 'server'
+    if parts[0] != "server":
+        return "Error: Command must start with 'server'.", None
 
+    # Check if '-t' is present and followed by a file name
+    if "-t" not in parts or len(parts) <= parts.index("-t") + 1:
+        return "Error: Missing topology file name.", None
 
-def execute_command(commands):
+    file_name = parts[parts.index("-t") + 1]
+    if not os.path.isfile(file_name):
+        return f"Error: File '{file_name}' does not exist.", None
+
+    # Check if '-i' is present and followed by a time interval
+    if "-i" not in parts or len(parts) <= parts.index("-i") + 1:
+        return "Error: Missing routing update interval.", None
+
     try:
-        command = commands.split()
-        if command[0] == 'help':
-            print("Print commands list here")  # debug
-            pass
-        elif command[0] == 'server':
-            print("Read topology file and update routing every x seconds")  # debug
-            pass
-        elif command[0] == 'update':
-            server_id_1, server_id_2, link_cost = int(command[1]), int(command[2]), int(command[3])
-            pass
-        elif command[0] == 'step':
-            pass
-        elif command[0] == 'packets':
-            pass
-        elif command[0] == 'display':
-            pass
-        elif command[0] == 'disable':
-            server_id = int(command[1])
-            pass
-        elif command[0] == 'crash':
-            pass
-    except KeyboardInterrupt:
-        exit_event.set()
-        exit()
-    except IndexError:
-        # not enough/too many args passed to update or disable command
-        pass
+        interval = int(parts[parts.index("-i") + 1])
+        if not 1 <= interval <= 10:
+            return "Error: Routing update interval must be between 1 and 10 seconds.", None
     except ValueError:
-        # args to update or disable command are not numbers
-        pass
+        return "Error: Routing update interval must be a number.", None
+
+    return "valid", file_name
 
 
 if __name__ == '__main__':
     program_manager = ProgramManager()
 
-    # ask for user command (only accept "server" at the beginning)
-    print("Enter 'server -t <topology-file-name> -i <routing-update-interval>' to startup the server.")
-    input_thread = threading.Thread(target=get_user_input)
-    input_thread.start()
-
-    # read topology file
+    # ask for user command (only accept "server" at the beginning),
+    # and read topology file
+    print("Enter 'server -t <topology-file-name> -i <routing-update-interval>' at the startup.")
+    while True:
+        try:
+            user_input = input("Enter a command: ")
+            validation_result, file_name = validate_command(user_input)
+            if validation_result == "valid":
+                # print("Command is valid.")  # debug
+                # print(f"Reading file: {file_name}")  # debug
+                # file_content = read_file(file_name)  # debug
+                # print("File content:")  # debug
+                # print(file_content)  # debug
+                print("host_id = program_manager.init_topology(file_name)")  # incomplete
+                print("get_update_interval")  # incomplete
+                break
+            else:
+                print(validation_result)
+                print("Please re-enter the command.")
+                print("'server -t <topology-file-name> -i <routing-update-interval>'")
+        except Exception as e:
+            print(f"{e}")
+            print("Please re-enter the command correctly.")
 
     # initialize this server's distance vector
 
@@ -79,4 +67,30 @@ if __name__ == '__main__':
 
     # start timer on another thread
 
-    # ask for user input on another thread
+    # ask for user input
+    while True:
+        try:
+            command = input('Enter a command: ').split()
+            if command[0] == 'update':
+                server_id_1, server_id_2, link_cost = int(command[1]), int(command[2]), int(command[3])
+                pass
+            elif command[0] == 'step':
+                pass
+            elif command[0] == 'packets':
+                pass
+            elif command[0] == 'display':
+                pass
+            elif command[0] == 'disable':
+                server_id = int(command[1])
+                pass
+            elif command[0] == 'crash':
+                pass
+        except KeyboardInterrupt:
+            exit_event.set()
+            exit()
+        except IndexError:
+            # not enough/too many args passed to update or disable command
+            pass
+        except ValueError:
+            # args to update or disable command are not numbers
+            pass
--- program_manager.py
+++ program_manager.py
@@ -1,9 +1,84 @@
+from server_object import ServerObject
+
+
 class ProgramManager:
-    list_of_servers = {} # server_id => ServerObject
+    list_of_servers = {}  # server_id => ServerObject
     update_interval = 0
+    our_server_id = None
+
+    def read_file(self, file_name):
+        try:
+            with open(file_name, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            print(f"Error: The file '{file_name}' was not found.")
+            return None
+        except IOError:
+            print(f"Error: An IO error occurred while reading '{file_name}'.")
+            return None
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return None
+
+    def validate_topology_file(self, lines, num_servers, num_edges):
+        # Check if the first two lines contain single numbers
+
+        if not (lines[0].isdigit() and lines[1].isdigit()):
+            print("Error: The first or second line does not contain a valid number.")
+            return False
+
+        # Check if the number of lines is correct
+        expected_line_count = 2 + num_servers + num_edges
+        if len(lines) != expected_line_count:
+            print("Error: The topology file does not contain the correct number of lines.")
+            return False
+
+        # Use a set to track the server IDs from each line
+        server_ids_sets = []
+
+        # Process connection lines and add server IDs to sets
+        connection_lines = lines[2 + num_servers:2 + num_servers + num_edges]
+        for line in connection_lines:
+            server_ids = set(map(int, line.split()[:2]))
+            server_ids_sets.append(server_ids)
+
+        # Find the common server ID across all lines
+        common_server_id = set.intersection(*server_ids_sets)
+
+        # Check if there is exactly one common server ID
+        if len(common_server_id) == 1:
+            self.our_server_id = common_server_id.pop()
+        else:
+            print("Error: Unable to determine 'our' server from the topology file.")
+            return False
+
+        return True
+
+    def parse_topology(self, content):
+        lines = content.split('\n')
+        num_servers = int(lines[0])
+        num_edges = int(lines[1])
+
+        # Validate the topology file
+        if not self.validate_topology_file(lines, num_servers, num_edges):
+            raise Exception("Invalid topology file.")
+
+        lines = content.split('\n')
+        for i in range(2, 2 + num_servers):
+            line = lines[i].split()
+            server_id = int(line[0])
+            ip = line[1]
+            port = int(line[2])
+            self.list_of_servers[server_id] = ServerObject(server_id, ip, port)
+
 
     def init_topology(self, file_name):
-        pass
+        content = self.read_file(file_name)
+        if not content:
+            raise Exception("Error: The topology file is empty or could not be read.")
+
+        self.parse_topology(content)
+        return self.our_server_id
 
     def get_server_by_id(self, server_id):
         pass
